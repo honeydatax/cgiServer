167e25971c29687fc7dc5731f401161c71f08a10
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-21 11:33:55 +0000

    Gcc -o loto loto.c

---
+++ lotary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	char ss[80];
+	char s[80];
+	int n;
+	int nn;
+	time_t tt;
+	tt=time(NULL);
+	strcpy(ss," ");
+	for(n=0;n<9;n++){
+		nn=(char) rand()/28;
+		sprintf(s,"%d",nn);
+		strcat(ss,s);
+	}
+	printf("Content-type:plain-text\r\n\r\n");
+	printf("\n%s\n",ss);
+	return 0;
+}
