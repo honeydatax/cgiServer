f22d493cc339304b066f5dae74becd81f8fe7588
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-22 16:25:04 +0000

    Gcc -o 1234567890 shutdown.c

---
+++ shutdown.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	printf("Content-type:plain-text\r\n\r\n\r\n\r\n\r\n\r");
+	system ("shutdown now");
+	printf("\r\n\r\n\r\n\r\n\r\n\r");
+	return 0;
+}
