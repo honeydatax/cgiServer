559c7ebd0103d1e2376cc60fbdf14ac420ec32b7
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-22 16:56:04 +0000

    Gcc -o 5555555555 stop.c

---
+++ stop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	printf("Content-type:plain-text\r\n\r\n\r\n\r\n\r\n\r");
+	system ("killall 'python'");
+	system ("killall 'start'");
+	printf("\r\n\r\n\r\n\r\n\r\n\r");
+	return 0;
+}
