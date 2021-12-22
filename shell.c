0ecc60f0993a8c696404807352fc546b6542f15f
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-22 22:58:41 +0000

    Gcc -o shell shell.c

--- shell.c
+++ shell.c
@@ -5,6 +5,19 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void *replacec(char *c){
+	int n;
+	int nn;
+	char cc;
+	nn=strlen(c);
+	for(n=0;n<nn;n++){
+		cc=c[n];
+		if (cc=='_'){
+			c[n]=' ';
+		}
+	}
+}
+
 int main(){
 	char envi[1080];
 	char *c;
@@ -12,6 +25,7 @@ int main(){
 	printf("Content-type:plain-text\r\n\r\n");
 	c=getenv(envi);
 	strcpy(envi,c);
+	replacec(envi);
 	strcat(envi," > siteMap.txt");
 	system(envi);
 	printf("%s\r\n",envi);
