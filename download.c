a5e6531c3bd6bcb9d7c00d05074aa9293336605f
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-23 12:18:04 +0000

    Curl http://localhost:8080/download?address

---
+++ download.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	char envi[1080];
+	char e[1080];
+	char ss[80];
+	char s[80];
+	char *c;
+	int n;
+	int nn;
+	FILE *f;
+	time_t tt;
+	tt=time(NULL);
+	srand(tt);
+		for(n=0;n<9;n++){
+		nn=(char)((char) rand())/28;
+		sprintf(s,"%d",nn);
+		strcat(ss,s);
+	}
+	strcpy(envi,"QUERY_STRING");
+	printf("Content-type:plain-text\r\n\r\n");
+	c=getenv(envi);
+	strcpy(envi,c);
+	sprintf(e,"timeout 15s curl http://%s > %s",envi,ss);
+	system(e);
+	printf("http:/localhost:8080/%s",ss);
+	return 0;
+}
