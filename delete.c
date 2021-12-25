99ede6a3661ffba383a836d8fd949fc8f58d70dc
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-12-25 18:59:32 +0000

    Add files via upload

---
+++ delete.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *replacec(char *c){
+	int n;
+	int nn;
+	char cc;
+	nn=strlen(c);
+	for(n=0;n<nn;n++){
+		cc=c[n];
+		if (cc=='_'){
+			c[n]=' ';
+		}
+	}
+}
+
+int main(){
+	FILE *f;
+	char envi[1080];
+	char *c;
+	strcpy(envi,"QUERY_STRING");
+	printf("Content-type:plain-text\r\n\r\n");
+	c=getenv(envi);
+	strcpy(envi,c);
+	replacec(envi);
+	f=fopen("/tmp/data.in","w");
+	fprintf(f,"delete from dt where ID > 0;\n",envi);
+	fprintf(f,"\n.quit\n");
+	fclose(f);
+	system("sqlite3 dd < /tmp/data.in");
+	printf("%s\r\n",envi);
+	return 0;
+}
